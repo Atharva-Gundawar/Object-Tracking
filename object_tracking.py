c5dab619c8e66b887fb4b9cb59aebdd7d142fff0
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-20 11:03:05 +0530

    Feat : BB_class

--- object_tracking.py
+++ object_tracking.py
@@ -1,41 +1,37 @@
 import cv2
 
+class BB_model :
 
-def init_model():
-    """[summary]
-
-    Returns:
-        [type]: [description]
-    """
-    face_cascade = cv2.CascadeClassifier(
-        'haarcascades\haarcascade_frontalface_default.xml')
-    return {"face_cascade": face_cascade}
+    def __init__(self):
+        """[summary]
+        """
+        face_cascade = cv2.CascadeClassifier(
+            'haarcascades\haarcascade_frontalface_default.xml')
+        self.face_cascade = face_cascade
 
 
-def get_bounding_boxs(frame, **kwargs):
-    """[summary]
+    def get_bounding_boxs(self, frame):
+        """[summary]
 
-    Args:
-        frame ([type]): [description]
+        Args:
+            frame ([type]): [description]
 
-    Returns:
-        [type]: [description]
-    """
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return kwargs.get('face_cascade').detectMultiScale(frame_gray)
+        Returns:
+            [type]: [description]
+        """
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return self.face_cascade.detectMultiScale(frame_gray)
 
 def main():
-    """[summary]
-    """
 
-    kwargs = init_model()
+    model = BB_model()
     cap = cv2.VideoCapture('Videos\WIN_20210619_10_22_40_Pro.mp4')
     while cap.isOpened():
         ret, frame = cap.read()
         # if frame is read correctly ret is True
         if not ret:
             break
-        boxes = get_bounding_boxs(frame, **kwargs)
+        boxes = model.get_bounding_boxs(frame)
         for (x, y, w, h) in boxes:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         cv2.imshow('frame', frame)
